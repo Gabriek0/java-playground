e05630c78bd77fe918756c1dc2f574d981cbe0aa
Gabriel <gabriel.sanches170@gmail.com>
2026-03-17 18:18:02 -0300

    feat: create Book model entity

---
+++ library-management-system/src/main/java/com/library_management/model/Book.java
@@ -0,0 +1,79 @@
+package main.java.com.library_management.model;
+
+import main.java.com.library_management.Genre;
+
+import java.util.UUID;
+
+public class Book {
+    private UUID id;
+    private String title;
+    private Author author;
+    private String isbn;
+    private int year;
+    private Genre genre;
+    private int availableCopies;
+
+    public Book(String title, Author author, String isbn, int year, Genre genre, int availableCopies) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.year = year;
+        this.genre = genre;
+        this.availableCopies = availableCopies;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre.toString();
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "ISBN: " + isbn + '\n' +
+                "Títle: " + title + '\n' +
+                "Year: " + year + '\n' +
+                "Genre: " + genre + '\n' +
+                "Available copies: " + availableCopies + '\n';
+    }
+}
