87d094e54c9f0fde70d3585df2f2e9b608ed788e
Gabriel <gabriel.sanches170@gmail.com>
2026-03-20 13:16:03 -0300

    feat: add getters and setter, change private attributes, and override toString method

--- library-management-system/src/main/java/com/library_management/model/Member.java
+++ library-management-system/src/main/java/com/library_management/model/Member.java
@@ -1,24 +1,51 @@
 package main.java.com.library_management.model;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.UUID;
 
 public class Member {
-    String id;
-    String email;
-    String name;
-    LocalDate registrationDate;
-    ArrayList<Loan> loans;
+    private String id;
+    private String email;
+    private String name;
+    private LocalDate registrationDate;
 
-    public Member(String id, String email, String name, LocalDate registrationDate, ArrayList<Loan> loans) {
-        this.id = id;
+    public Member(String email, String name, LocalDate registrationDate) {
+        this.id = UUID.randomUUID().toString();
         this.email = email;
         this.name = name;
         this.registrationDate = registrationDate;
-        this.loans = loans;
     }
 
-    public boolean isElegibleToLoan() {
-        return loans.size() < 3;
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + '\n' + "Name: " + name + '\n' + "Email: " + email + '\n' + "Registration date: " + registrationDate;
     }
 }
