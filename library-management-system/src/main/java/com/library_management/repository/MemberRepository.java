61520d67f3830650e3c9e44602692b5f6953ae73
Gabriel <gabriel.sanches170@gmail.com>
2026-03-20 13:18:08 -0300

    feat: create MemberRepository

--- library-management-system/src/main/java/com/library_management/repository/MemberRepository.java
+++ library-management-system/src/main/java/com/library_management/repository/MemberRepository.java
@@ -1,4 +1,32 @@
 package main.java.com.library_management.repository;
 
-public class MemberRepository {
+import main.java.com.library_management.interfaces.Repository;
+import main.java.com.library_management.model.Member;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class MemberRepository implements Repository<Member, String> {
+    private ArrayList<Member> members = new ArrayList<>();
+
+    public void create(Member item) {
+        members.add(item);
+    }
+
+    public void update(String id, Member item) {
+        members.removeIf(m -> m.getId().equals(id));
+        members.add(item);
+    }
+
+    public void delete(String id) {
+        members.removeIf(m -> m.getId().equals(id));
+    }
+
+    public Optional<Member> findById(String id) {
+        return members.stream().filter(b -> b.getId().equals(id)).findFirst();
+    }
+
+    public ArrayList<Member> list() {
+        return members;
+    }
 }
