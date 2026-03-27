fb71a94f7c48d9d094d623f4e78b82c99218fbdc
Gabriel <gabriel.sanches170@gmail.com>
2026-03-27 09:30:45 -0300

    feat: create Loan entity

--- library-management-system/src/main/java/com/library_management/model/Loan.java
+++ library-management-system/src/main/java/com/library_management/model/Loan.java
@@ -1,23 +1,67 @@
 package main.java.com.library_management.model;
 
+import main.java.com.library_management.enums.LoanStatus;
+
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Loan {
-    Book book;
-    Member member;
-    LocalDate loanDate;
-    LocalDate dueDate;
-    LocalDate returnDate;
+    private String id;
+    private Book book;
+    private Member member;
+    private LoanStatus loanStatus;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
 
-    public Loan(Book book, Member member, LocalDate loanDate, LocalDate dueDate, LocalDate returnDate) {
+    public Loan(Book book, Member member, LoanStatus loanStatus, LocalDate loanDate, LocalDate dueDate, LocalDate returnDate) {
+        this.id = UUID.randomUUID().toString();
         this.book = book;
         this.member = member;
         this.loanDate = loanDate;
+        this.loanStatus = loanStatus;
         this.dueDate = dueDate;
         this.returnDate = returnDate;
     }
 
-    public boolean isOverdue() {
-        return dueDate.isBefore(returnDate);
+    public String getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setLoanStatus(LoanStatus loanStatus) {
+        this.loanStatus = loanStatus;
+    }
+
+    public LoanStatus getLoanStatus() {
+        return loanStatus;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + '\n' + book.toString() + member.toString() + "Loan Date: " + loanDate + '\n' + "Due Date: " + dueDate + '\n' + "Return Date: " + returnDate + '\n';
     }
 }
