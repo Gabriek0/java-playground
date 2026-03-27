251814493366c0acb0b6993360bac5543b72321e
Gabriel <gabriel.sanches170@gmail.com>
2026-03-27 09:31:48 -0300

    feat: create LoanRepository

--- library-management-system/src/main/java/com/library_management/repository/LoanRepository.java
+++ library-management-system/src/main/java/com/library_management/repository/LoanRepository.java
@@ -1,4 +1,32 @@
 package main.java.com.library_management.repository;
 
-public class LoanRepository {
+import main.java.com.library_management.interfaces.Repository;
+import main.java.com.library_management.model.Loan;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class LoanRepository implements Repository<Loan, String> {
+    private ArrayList<Loan> loans = new ArrayList<>();
+
+    public void create(Loan item) {
+        loans.add(item);
+    }
+
+    public void update(String id, Loan item) {
+        loans.removeIf(loan -> loan.getId().equals(id));
+        loans.add(item);
+    }
+
+    public void delete(String id) {
+        loans.removeIf(loan -> loan.getId().equals(id));
+    }
+
+    public Optional<Loan> findById(String id) {
+        return loans.stream().filter(loan -> loan.getId().equals(id)).findFirst();
+    }
+
+    public ArrayList<Loan> list() {
+        return loans;
+    }
 }
