b440ae1a12198058f7cae13c923f1bbf0892f779
Gabriel <gabriel.sanches170@gmail.com>
2026-03-30 13:48:30 -0300

    feat(LoanService): add rules and feedback messages

--- library-management-system/src/main/java/com/library_management/service/LoanService.java
+++ library-management-system/src/main/java/com/library_management/service/LoanService.java
@@ -6,10 +6,11 @@ import main.java.com.library_management.model.Book;
 import main.java.com.library_management.model.Loan;
 import main.java.com.library_management.model.Member;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.time.LocalDate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LoanService {
     private static final int MIN_LOAN_DAYS = 14;
@@ -26,53 +27,80 @@ public class LoanService {
     }
 
     public void createLoan(String isbn, String memberId) {
-        if (isExceededLoans(memberId)) {
-            System.out.println("The user has exceeded the limit of " + MAX_LOANS_AMOUNT + " loans");
-            return;
-        }
-
         Optional<Book> book = bookService.findByIsbn(isbn);
 
         if (book.isEmpty()) {
-            System.out.println("The book is not available for loan");
+            System.out.println("No book found with ISBN " + isbn);
             return;
         }
 
         if (book.get().getAvailableCopies() == 0) {
-            System.out.println("There are no copies of this book available for loan");
+            System.out.println("There are no available copies of this book");
             return;
         }
 
         Optional<Member> member = memberService.findById(memberId);
 
         if (member.isEmpty()) {
-            System.out.println("The user does not exist");
+            System.out.println("No member found with ID " + memberId);
             return;
         }
 
+        if (isExceededLoans(memberId)) {
+            System.out.println("This member has reached the maximum loan limit of " + MAX_LOANS_AMOUNT + " books");
+            return;
+        }
 
         Loan loan = new Loan(book.get(), member.get(), LoanStatus.BORROWED, LocalDate.now(), LocalDate.now().plusDays(MIN_LOAN_DAYS), null);
         loanRepository.create(loan);
 
         book.get().setAvailableCopies(book.get().getAvailableCopies() - 1);
         bookService.updateBook(book.get());
+
+        System.out.println(loan);
+        System.out.println("Loan successfully created!");
     }
 
     public void returnBook(String loanId) {
         Optional<Loan> loan = findLoanById(loanId);
 
         if (loan.isEmpty()) {
-            System.out.println("No loans were found with the ID " + loanId);
+            System.out.println("No loan found with ID " + loanId);
             return;
         }
 
         loan.get().setLoanStatus(LoanStatus.RETURNED);
         loan.get().setReturnDate(LocalDate.now());
-        updateLoan(loan.get());
+        loanRepository.update(loan.get().getId(), loan.get());
 
         Book book = loan.get().getBook();
         book.setAvailableCopies(book.getAvailableCopies() + 1);
         bookService.updateBook(book);
+
+        System.out.println(loan.get());
+        System.out.println("Book successfully returned!");
+    }
+
+    public void updateLoan(Loan item) {
+        if (findLoanById(item.getId()).isEmpty()) {
+            System.out.println("No loan found with ID " + item.getId());
+            return;
+        }
+
+        loanRepository.update(item.getId(), item);
+
+        System.out.println(item);
+        System.out.println("Loan successfully updated!");
+    }
+
+    public void removeLoan(String id) {
+        if (findLoanById(id).isEmpty()) {
+            System.out.println("No loan found with ID " + id);
+            return;
+        }
+
+        loanRepository.delete(id);
+        System.out.println("Loan successfully removed!");
     }
 
     public ArrayList<Loan> listLoans() {
@@ -83,21 +111,14 @@ public class LoanService {
         return listLoans().stream().filter(loan -> loan.getLoanStatus() == LoanStatus.BORROWED).collect(Collectors.toList());
     }
 
-    public void updateLoan(Loan item) {
-        loanRepository.update(item.getId(), item);
-    }
-
-    public void removeLoan(String id) {
-        loanRepository.delete(id);
-    }
-
     public Optional<Loan> findLoanById(String id) {
         return loanRepository.findById(id);
     }
 
     private boolean isExceededLoans(String memberId) {
-        Stream<Loan> loanStream = listLoans().stream().filter(loan -> loan.getMember().getId().equals(memberId));
-        long loansBorrowed = loanStream.filter(loan -> loan.getLoanStatus() == LoanStatus.BORROWED).count();
-        return loansBorrowed >= MAX_LOANS_AMOUNT;
+        return listLoans().stream()
+                .filter(loan -> loan.getMember().getId().equals(memberId))
+                .filter(loan -> loan.getLoanStatus() == LoanStatus.BORROWED)
+                .count() >= MAX_LOANS_AMOUNT;
     }
 }
